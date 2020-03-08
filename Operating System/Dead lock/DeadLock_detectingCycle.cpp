18cb15aa35e161de7dbdce3ad380befc41c3ec27
fuadsaft <fuadsafat16@gmail.com>
2020-03-08 22:28:25 +0600

    Detection cycle of deadlock

---
+++ Operating System/Dead lock/DeadLock_detectingCycle.cpp
@@ -0,0 +1,72 @@
+
+#include<bits/stdc++.h>
+
+using namespace std;
+int n,e;
+bool flag = false;
+int cycle[100];
+vector<int>V[100];
+int color[100];
+
+int mainNode,k;
+
+void printTheCycle()
+{
+    for(int i = 0; i < k; i++)
+        cout<<cycle[i]<<" ";
+    cout<<"\n";
+}
+
+void DFS(int u)
+{
+    color[u] = 1;
+    cycle[k++] = u;
+    for(int i = 0; i < V[u].size(); i++)
+    {
+        int v = V[u][i];
+        if(v == mainNode)
+        {
+            cout<<"A cycle detected\n";
+            printTheCycle();
+            return;
+        }
+        else if(color[v] == 0)
+        {
+            DFS(v);
+        }
+    }
+}
+
+
+int main()
+{
+
+    cin>>n>>e;
+    for(int i = 1; i<= e; i++)
+    {
+        int u,v;
+        cin>>u>>v;
+        V[u].push_back(v);
+    }
+
+    for(int i = 1; i <= n; i++)
+    {
+
+        if(color[i] == 0)
+        {
+            k = 0;
+            mainNode = i;
+            DFS(i);
+        }
+
+    }
+
+
+
+
+
+    return 0;
+
+
+
+}
