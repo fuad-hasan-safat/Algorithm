ab473472fb535a93e8359e733eb129dbff573a06
fuadsaft <fuadsafat16@gmail.com>
2020-03-09 23:25:57 +0600

    recovery updated

--- Operating System/Dead lock/DealLock__recovery.cpp
+++ Operating System/Dead lock/DealLock__recovery.cpp
@@ -4,8 +4,9 @@ void printTheCycle();
 
 
 int isRes[100];
+bool bg = false;
 
-int node, edge,k, mainNode;
+int node, edge,k, mainNode,m;
 vector<int> V[100];
 int color[100];
 int cycle[100];
@@ -13,61 +14,72 @@ int resource, process;
 
 
 
-void killAndRecovery()
+void killAndRecovery(int m)
 {
     // find resource and
-
-
-    if(isRes[cycle[0]])
-        resource = cycle[0];
+   // cout<<"Enter kill process\n";
+    if(isRes[cycle[m]])
+        resource = cycle[m];
     else
-        process = cycle[0];
+        process = cycle[m];
 
     if(isRes[cycle[k-1]])
         resource = cycle[k-1];//
     else
         process = cycle[k-1];
 
-    cout<<"Resource: "<<resource;
-    cout<<",process: "<<process<<"\n";
+    //cout<<"Resource: "<<resource;
+    //cout<<",process: "<<process<<"\n";
     cout<<"process "<<process<<" is killed\n";
 
-    V[resource][process] = -1;
-    cycle[process] = -1;
+    V[resource].clear();
 
-    for(int i = 0; i < k; i++)
+    for(int i = m; i < k; i++)
         if(cycle[i] != process)
             cout<<cycle[i]<<" ";
     cout<<"\n";
 
 }
 
-void printTheCycle()
+void printTheCycle(int m)
 {
-    for(int i = 0; i < k; i++)
-            cout<<cycle[i]<<" ";
+    //cout<<"Enter PrintCycle\n";
+    for(int i = m; i < k; i++)
+        cout<<cycle[i]<<" ";
     cout<<"\n";
 
-    killAndRecovery();
+    killAndRecovery(m);
+    return;
 }
 
 
 
 void DFS(int u)
 {
+    //cout<<"Enter dfs\n";
+    //cout<<"u = "<<u<<"\n";
+
     color[u] = 1;
     cycle[k++] = u;
     for(int i = 0; i < V[u].size(); i++)
     {
         int v = V[u][i];
+        //cout<<"v = "<<v<<"\n";
+        //cout<<"main node = "<<mainNode<<"\n";
         if(v == mainNode)
         {
             cout<<"A cycle detected\n";
-            printTheCycle();
-            return;
+            printTheCycle(m);
+            m = k;
+            bg = true;
         }
         else if(color[v] == 0)
         {
+            if(bg)
+            {
+                bg = false;
+                mainNode = v;
+            }
             DFS(v);
         }
     }
@@ -98,9 +110,10 @@ int main()
 
         if(color[i] == 0)
         {
+            m = 0;
             k = 0;
             mainNode = i;
-            DFS(i);
+            DFS(mainNode);
         }
     }
 
@@ -123,6 +136,18 @@ int main()
 11 12 1 0
 12 13 0 1
 13 7 1 0
+
+
+
+
+1 2 0 1
+2 3 1 0
+3 1 0 0
+2 4 1 0
+4 5 0 1
+5 6 1 0
+6 7 0 1
+7 4 1 0
 */
 
 //
